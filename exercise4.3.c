fb12bca24be0de43968e3646cf65df351b4c8c7d
evan <evanchime@yahoo.co.uk>
2018-08-09 01:03:58 +0100

    Modified exercise4.3.c and a.out

--- exercise4.3.c
+++ exercise4.3.c
@@ -5,12 +5,12 @@ int
 main(void) 
 { 
 	struct stat statbuf; /* turn on set-group-ID and turn off group-execute */ 
-	if (stat("foo", &statbuf) < 0) 
+	if (stat("goo", &statbuf) < 0) 
 		err_sys("stat error for foo"); 
 	/*if (chmod("foo", (statbuf.st_mode & ˜S_IXGRP) | S_ISGID) < 0) 
 		err_sys("chmod error for foo"); // set absolute mode to "rw-r--r--"  
 	if (chmod("bar", S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) < 0) 
 		err_sys("chmod error for bar"); */
-	if (chmod("foo", (statbuf.st_mode & ~S_IRUSR)) < 0)                                                                                           err_sys("chmod error for foo"); 	
+	if (chmod("goo", (statbuf.st_mode & ~S_IRUSR)) < 0)                                                                                           err_sys("chmod error for foo"); 	
 	exit(0); 
 }
