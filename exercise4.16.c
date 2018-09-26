4ccc83302b796e42eb387aa45e8a658b8a5c01ed
evan <evanchime@yahoo.co.uk>
2018-09-27 00:10:53 +0200

    Added some comments to exercise4.16.c

--- exercise4.16.c
+++ exercise4.16.c
@@ -1,3 +1,10 @@
+/*
+ 	*Does the UNIX System have a fundamental limitation on the depth of a directory tree? 
+	*To ﬁnd out, write a program that creates a directory and then changes to that directory, in a loop. 
+	*Make certain that the length of the absolute pathname of the leaf of this directory is greater than your system’s PATH_MAX limit. 
+	*Can you call getcwd to fetch the directory’s pathname?
+*/ 
+
 #include "standardErrorRoutines.h"
 #include "path_alloc.h"
 
@@ -9,11 +16,11 @@ main(void)
 	char *buf; // buffer for getcwd call
 
 
-        buf = path_alloc(&size);
+        buf = path_alloc(&size); // return value of PATH_MAX, and allocate buffer of PATH_MAX size
 
 	size_copy = size;
 
-	while(size-- != 0){
+	while(size-- != 0){ // creates a directory and then changes to that directory, in a loop
 		if(mkdir("a", DIR_MODE) < 0){
 			err_sys("mkdir failed");
 		}	
