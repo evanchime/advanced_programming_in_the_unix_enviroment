98055f2faaa9a44ea42f9c187b49d3e311a3a041
Evan Chime <evanchime@yahoo.co.uk>
2018-12-12 12:19:01 +0000

    Update exercise5.1.h to correct line buffering feature-line buffering to occur only when connected to stdin/out/err

--- exercise5.1.h
+++ exercise5.1.h
@@ -11,10 +11,7 @@ my_setbuf(FILE *restrict fp,char *restrict buf){
 		size = 0;
 		mode = _IONBF;
 	}else{ // set line buffering if the stream is associated with a terminal device, otherwise full buffering
-		struct stat sbuf;
-		if(fstat(fileno(fp), &sbuf) < 0)
-			err_ret("fstat error");
-		else if(S_ISBLK(sbuf.st_mode) || fp == stdin || fp == stdout || fp == stderr)
+		if(fp == stdin || fp == stdout || fp == stderr)
 			mode = _IOLBF;
 	}
 	setvbuf(fp, buf, mode, size);
