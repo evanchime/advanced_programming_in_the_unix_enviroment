b6ca0892cc7dcbc686be1182475f006623ad3e19
evan <evanchime@yahoo.co.uk>
2018-12-24 02:13:01 +0100

    Modified exercise6.3.c and consequently a.out

--- exercise6.3.c
+++ exercise6.3.c
@@ -1,3 +1,8 @@
+/*
+	* Write a program that calls uname and prints all the ﬁelds in the utsname structure. 
+	* Compare the output to the output from the uname(1) command.
+*/
+
 #include "apue.h"
 #include "standarderrorroutines.h"
 #include <sys/utsname.h>
@@ -13,7 +18,7 @@ int
 main(int argc, char *argv[])
 {
 	struct utsname name;
-	int options;
+	int options = 0;
 	int c;
 
 	if(uname(&name) < 0)
@@ -25,8 +30,6 @@ main(int argc, char *argv[])
 		while (--argc > 0 && (*++argv)[0] == '-'){
      			int flag = 1; // check against illegal option
      			while (c = *++argv[0]){
-            			if(flag == 0)
-                			err_sys("uname: illegal option");
             			switch (c) {
                 			case 'a':
                     				options = ALL;
@@ -47,24 +50,8 @@ main(int argc, char *argv[])
                     				options |= OP_VERSION;
                     				break;
                 			case '-':
-                    				/*char *s; 
-						s = ++argv[0];
-                    				if(strcmp(s, "all") == 0)
-                        				options = ALL;
-                    				else if(strcmp(s, "nodename") == 0)
-                        				options |= OP_NODENAME;
-                    				else if(strcmp(s, "machine") == 0)
-                        				options |= OP_MACHINE;
-                    				else if(strcmp(s, "operating-system") == 0)
-                        				options |= OP_SYSNAME;
-                    				else if(strcmp(s, "version") == 0)
-                        				options |= OP_VERSION;
-                    				else if(strcmp(s, "release") == 0)
-                        				options |= OP_RELEASE;
-                    				else
-                        				err_sys("uname: illegal option");
-                    				argv[0] += (strlen(argv[0]) - 1); 
-                    				break;*/
+                    				if(flag ^= 1)
+                					err_sys("uname: illegal option");
 						{
                         				char *s = ++argv[0];
                         				if(strcmp(s, "all") == 0)
@@ -91,12 +78,26 @@ main(int argc, char *argv[])
     			}
 		}
 	}
+
+	if(options & OP_NODENAME){
+    		printf("nodename = %s\n", name.nodename);
+	}
 	
-	printf((options & OP_NODENAME) ? "nodename = %s\n" : "", name.nodename);
-	printf((options & OP_MACHINE) ? "machine = %s\n" : "", name.machine);
-	printf((options & OP_SYSNAME) ? "operating-system = %s\n" : "", name.sysname);
-	printf((options & OP_VERSION) ? "version = %s\n" : "", name.version);
-	printf((options & OP_RELEASE) ? "release = %s\n" : "", name.release);
+	if(options & OP_MACHINE){
+    		printf("machine = %s\n", name.machine);
+	}
+
+	if(options & OP_SYSNAME){
+    		printf("operating-system = %s\n", name.sysname);
+	}
+
+	if(options & OP_VERSION){
+    		printf("version = %s\n", name.version);
+	}
+
+	if(options & OP_RELEASE){
+    		printf("release = %s\n", name.release);
+	}	
 	
 	exit(0);
 }
