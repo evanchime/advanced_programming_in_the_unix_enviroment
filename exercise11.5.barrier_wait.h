db28c8f580a3c85014d1929be7747e70497abc4d
Evan Chime <evanchime@yahoo.co.uk>
2019-03-22 11:43:20 +0000

    Added some new features

--- exercise11.5.barrier_wait.h
+++ exercise11.5.barrier_wait.h
@@ -1,5 +1,13 @@
-pthread_cond_t bready = PTHREAD_COND_INITIALIZER; 
-pthread_mutex_t balock = PTHREAD_MUTEX_INITIALIZER;
+int 
+pthread_barrier_init(pthread_barrier_t *restrict barrier, const pthread_barrierattr_t *restrict attr, unsigned int count){
+    if(pthread_mutex_init(&x.balock, (const pthread_mutexattr_t *)attr) != 0)
+        return errno;
+    x.tcount = count;
+    x.flag = 1;
+    if(pthread_cond_init((pthread_cond_t *)barrier, (const pthread_condattr_t *)attr)!= 0)
+        return errno;
+    return 0;
+}
 
 struct {
     pthread_mutex_t balock;
@@ -7,7 +15,8 @@ struct {
     int flag;
 }x;
 
-int pthread_barrier_wait(pthread_barrier_t *barrier){
+int 
+pthread_barrier_wait(pthread_barrier_t *barrier){
     static tcount = 1;
     pthread_mutex_lock(&x.balock); 
     while (tcount != x.tcount){ 
@@ -19,10 +28,11 @@ int pthread_barrier_wait(pthread_barrier_t *barrier){
         pthread_cond_broadcast((pthread_cond_t *)barrier);
     }
     pthread_mutex_unlock(&x.balock);
-    
-}
+ }
 
-
-int pthread_cond_wait(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex){
-
+int 
+pthread_barrier_destroy(pthread_barrier_t *barrier){
+    if(pthread_cond_destroy((pthread_cond_t *)barrier) != 0)
+        return errno;
+     return 0;
 }
