e8fbd792080d35d19b4aa0d1ba61534abbacc277
evanchime <evanchime@yahoo.co.uk>
2019-08-08 16:33:54 +0100

    unfinished version of exercise13.4.c

--- exercise13.4.c
+++ exercise13.4.c
@@ -9,7 +9,15 @@ main(int argc, char *argv[]){
 	FILE *fp;
         if((fp = fopen(argv[1], "w+") == NULL))
 	        err_sys("fopen failed");
+	/*int fd;
+	if ((fd = open(argv[1], O_RDWR|O_CREAT|O_TRUNC, FILE_MODE)) < 0) 
+		err_sys("open error");*/
+	fprintf(fp, "before daemon");
 	daemonize(argv[0]); //daemonize
+	/*FILE *fp;
+        if((fp = fopen(argv[1], "w+") == NULL))
+                err_sys("fopen failed");*/
+	fprintf(fp, "in daemon now");
 	if((name = getlogin()) == NULL)
 		fprintf(fp, "can't daemonize");
 	else
