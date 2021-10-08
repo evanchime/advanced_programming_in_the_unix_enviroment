8cb8025a4a1ed3db1703c96cf39e067a96aaf1cb
Evan Chime <chime.evan.dri.devel@gmail.com>
2021-10-08 17:45:12 +0100

    Add files for LFCS Practice Questions

---
+++ scan_filesystem.c
@@ -0,0 +1,16 @@
+#include "/home/ik/advanced_programming_in_the_unix_enviroment/apue.h"
+#include "/home/ik/advanced_programming_in_the_unix_enviroment/standarderrorroutines.h"
+
+
+int
+main(void)
+{
+	
+	for(;;){
+		printf("hello world\n");
+		sleep(5);
+	}
+
+	exit(0);
+
+}
